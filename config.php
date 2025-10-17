e84bf919d5ef4cf0cbfa1ec8c983f3f8c282753c
IBertoo <alberto.marcani@gmail.com>
2025-10-17 14:01:56 -0400

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 // Lee variables de entorno inyectadas por docker-compose
 $CONFIG = [
-  'db_host' => getenv('DB_HOST') ?: 'sql206.infinityfree.com',
-  'db_name' => getenv('DB_NAME') ?: 'if0_40132648_catalogo',
-  'db_user' => getenv('DB_USER') ?: 'if0_40132648',
-  'db_pass' => getenv('DB_PASSWORD') ?: 'iAmPprFD1Uo',
+  'db_host' => getenv('DB_HOST') ?: '',
+  'db_name' => getenv('DB_NAME') ?: '',
+  'db_user' => getenv('DB_USER') ?: '',
+  'db_pass' => getenv('DB_PASSWORD') ?: '',
   'app_name' => 'Catálogo',
 ];
